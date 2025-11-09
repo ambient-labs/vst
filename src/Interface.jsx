c3108102aac6951e7c1a61e46cd637fb1833a484
Ambient Labs <178958084+ambient-labs@users.noreply.github.com>
2025-11-09 13:50:03 -0500

    Remove error alert

--- src/Interface.jsx
+++ src/Interface.jsx
@@ -6,35 +6,6 @@ import Knob from './Knob.jsx';
 import manifest from '../public/manifest.json';
 
 
-// Generated from Lockup.svg using svgr, and then I changed the generated code
-// a bit to use a currentColor fill on the text path, and to move the strokeLinejoin/cap
-// style properties to actual dom attributes because somehow that was causing problems
-function ErrorAlert({message, reset}) {
-  return (
-    <div className="rounded-md bg-red-50 p-4">
-      <div className="flex">
-        <div className="flex-shrink-0">
-          <XCircleIcon className="h-5 w-5 text-red-400" aria-hidden="true" />
-        </div>
-        <div className="ml-3">
-          <p className="text-sm font-medium text-red-800">{message}</p>
-        </div>
-        <div className="ml-auto pl-3">
-          <div className="-mx-1.5 -my-1.5">
-            <button
-              type="button"
-              onClick={reset}
-              className="inline-flex rounded-md bg-red-50 p-1.5 text-red-500 hover:bg-red-100 focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-offset-2 focus:ring-offset-red-50"
-            >
-              <span className="sr-only">Dismiss</span>
-              <XMarkIcon className="h-5 w-5" aria-hidden="true" />
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
-}
 
 // The interface of our plugin, exported here as a React.js function
 // component.
